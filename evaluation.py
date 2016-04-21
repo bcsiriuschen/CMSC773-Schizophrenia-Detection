c43f83b783e6b6a3fa210f022b4a2c3e065bdd9f
bcsiriuschen <bcsiriuschen@gmail.com>
2016-04-20 21:46:03 -0400

    change evaliation code

--- evaluation.py
+++ evaluation.py
@@ -1,11 +1,6 @@
 import os
-if __name__ == '__main__':
 
-    # Input Parameters
-    input_file = 'vw.output'
-    groundtruth_file = 'groundtruth.output'
-    output_file = 'vw.tpfp'
-
+def compute_accuracy(input_file, groundtruth_file):
     #Compute 0/1 Accuracy
     result = [float(x) for x in open(input_file).readlines()]
     groundtruth = [float(x) for x in open(groundtruth_file).readlines()]
@@ -14,9 +9,14 @@ if __name__ == '__main__':
     for (i, score) in enumerate(result):
         if float(score)*float(groundtruth[i]) > 0:
             correct += 1
-    print '0/1 Accuracy: %.4f'%(float(correct)/total)
+    return float(correct)/total
 
+
+def compute_auc(input_file, groundtruth_file):
     #Generate TP-FP pairs for ROC curve and AUC
+    result = [float(x) for x in open(input_file).readlines()]
+    groundtruth = [float(x) for x in open(groundtruth_file).readlines()]
+    total = len(result)
     total_positive = sum([1 for x in groundtruth if x > 0])
     total_negative = sum([1 for x in groundtruth if x < 0])
     curr_pos_count = 0
@@ -24,7 +24,6 @@ if __name__ == '__main__':
     true_positive = []
     false_positive = []
     sorted_groundtruth = [groundtruth[i] for i in sorted(range(total), key=lambda i: result[i], reverse=True)]
-    fp = open(output_file, 'w')
     for label in sorted_groundtruth:
         if label > 0:
             curr_pos_count += 1
@@ -32,14 +31,33 @@ if __name__ == '__main__':
             curr_neg_count += 1
             true_positive.append(float(curr_pos_count)/total_positive)
             false_positive.append(float(curr_neg_count)/total_negative)
-    for i, tp in enumerate(true_positive):
-        fp.write('%f %f\n'%(tp, false_positive[i]))
+    
     
     #Compute AUC
     temp1 = [x-y for (x,y) in zip(false_positive[1:],false_positive[:-1])]
     temp2 = [x+y for (x,y) in zip(true_positive[1:], true_positive[:-1])]
     auc = 0.5*sum([x*y for (x,y) in zip(temp1, temp2)]);
+
+    return (auc, true_positive, false_positive)
+    
+
+if __name__ == '__main__':
+
+    # Input Parameters
+    input_file = 'vw.output'
+    groundtruth_file = 'groundtruth.output'
+    output_file = 'vw.tpfp'
+
+    #Compute 0/1 Accuracy
+    acc = compute_accuracy(input_file, groundtruth_file)
+    print '0/1 Accuracy: %.4f'%(acc)
+    #compute AUC
+    [auc, true_positive, false_positive] = compute_auc(input_file, groundtruth_file)
     print 'AUC: %.4f'%(auc)
 
-    #plot ROC curve using gnuplot
-    os.system('gnuplot %s'%(output_file))
+    #SAVE TP-FP pairs into files for gnuplot to draw ROC curve
+    fp = open(output_file, 'w')
+    for i, tp in enumerate(true_positive):
+        fp.write('%f %f\n'%(tp, false_positive[i]))
+    fp.close()
+    os.system('gnuplot plot_roc.gnuplot')
